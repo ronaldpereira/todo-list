12905266722b461c36fa258bb67c776a19e1d914
Ronald Pereira <ronald.drp11@gmail.com>
2016-12-15 20:58:06 -0200

    Edited the auth page and got some css libraries into the project

--- public/auth/js/index.js
+++ public/auth/js/index.js
@@ -3,7 +3,7 @@ var LoginModalController = {
     tabElementName: ".logmod__tab",
     inputElementsName: ".logmod__form .input",
     hidePasswordName: ".hide-password",
-    
+
     inputElements: null,
     tabsElement: null,
     tabElement: null,
@@ -11,54 +11,54 @@ var LoginModalController = {
     
     activeTab: null,
     tabSelection: 0, // 0 - first, 1 - second
-    
+
     findElements: function () {
         var base = this;
-        
+
         base.tabsElement = $(base.tabsElementName);
         base.tabElement = $(base.tabElementName);
         base.inputElements = $(base.inputElementsName);
         base.hidePassword = $(base.hidePasswordName);
-        
+
         return base;
     },
-    
+
     setState: function (state) {
-    	var base = this,
-            elem = null;
-        
+        var base = this,
+        elem = null;
+
         if (!state) {
             state = 0;
         }
-        
+
         if (base.tabsElement) {
-        	elem = $(base.tabsElement[state]);
+            elem = $(base.tabsElement[state]);
             elem.addClass("current");
             $("." + elem.attr("data-tabtar")).addClass("show");
         }
-  
+
         return base;
     },
-    
+
     getActiveTab: function () {
         var base = this;
-        
+
         base.tabsElement.each(function (i, el) {
-           if ($(el).hasClass("current")) {
-               base.activeTab = $(el);
-           }
+            if ($(el).hasClass("current")) {
+                base.activeTab = $(el);
+            }
         });
-        
+
         return base;
     },
-   
+
     addClickEvents: function () {
-    	var base = this;
-        
+        var base = this;
+
         base.hidePassword.on("click", function (e) {
             var $this = $(this),
-                $pwInput = $this.prev("input");
-            
+            $pwInput = $this.prev("input");
+
             if ($pwInput.attr("type") == "password") {
                 $pwInput.attr("type", "text");
                 $this.text("Hide");
@@ -67,15 +67,15 @@ var LoginModalController = {
                 $this.text("Show");
             }
         });
- 
+
         base.tabsElement.on("click", function (e) {
             var targetTab = $(this).attr("data-tabtar");
-            
+
             e.preventDefault();
             base.activeTab.removeClass("current");
             base.activeTab = $(this);
             base.activeTab.addClass("current");
-            
+
             base.tabElement.each(function (i, el) {
                 el = $(el);
                 el.removeClass("show");
@@ -84,20 +84,20 @@ var LoginModalController = {
                 }
             });
         });
-        
+
         base.inputElements.find("label").on("click", function (e) {
-           var $this = $(this),
-               $input = $this.next("input");
-            
+            var $this = $(this),
+            $input = $this.next("input");
+
             $input.focus();
         });
-        
+
         return base;
     },
-    
+
     initialize: function () {
         var base = this;
-        
+
         base.findElements().setState().getActiveTab().addClickEvents();
     }
 };
--- server.js
+++ server.js
@@ -9,11 +9,11 @@ var port     = process.env.PORT || 8080;
 
 mongoose.connect(database.url);
 
-app.use(express.static(__dirname + '/public/todolist'));                 // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                         // log every request to the console
-app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(express.static(__dirname + '/public/auth'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
 // routes
