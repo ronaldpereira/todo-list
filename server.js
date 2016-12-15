f0e8d5b61807c1f618eb24e89ad58f84522c5beb
Ronald Pereira <ronald.drp11@gmail.com>
2016-12-15 15:36:05 -0200

    server.js goes to the todolist index page - Later it will go to the auth page

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ var port     = process.env.PORT || 8080;
 
 mongoose.connect(database.url);
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public/todolist'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-// routes ======================================================================
+// routes
 require('./app/routes.js')(app);
 
-// listen (start app with node server.js) ======================================
+// listen (start app with node server.js)
 app.listen(port);
 console.log("App listening on port : " + port);
